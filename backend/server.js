8f6fbdf2c872dd16f8b15cabc3337719669993b3
vedagnya <vedagnya.kanchu@gmail.com>
2026-02-08 20:38:49 +0530

    Fix POST request issue and refactor backend structure

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,20 @@
 const express = require("express");
 const app = express();
-const PORT = 5000;
 
-// middleware to parse JSON
+// VERY IMPORTANT
 app.use(express.json());
 
-// import routes
-const healthRoutes = require("./routes/health.routes");
+app.post("/test", (req, res) => {
+  res.status(200).json({
+    message: "POST working",
+    body: req.body,
+  });
+});
 
-// use routes
-app.use("/api", healthRoutes);
+app.get("/test", (req, res) => {
+  res.send("GET working");
+});
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+app.listen(5000, () => {
+  console.log("✅ Server running on port 5000");
 });
